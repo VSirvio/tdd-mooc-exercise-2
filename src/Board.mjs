86d5411310df6173d48d482cb40178c741b93dc4
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-10 15:05:57 +0300

    refactor: add gameArea attribute to Board

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,10 +1,19 @@
 export class Board {
   width;
   height;
+  gameArea;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+
+    this.gameArea = [];
+    for (let y = 0; y < height; ++y) {
+      this.gameArea.push([]);
+      for (let x = 0; x < width; ++x) {
+        this.gameArea[y].push('.');
+      }
+    }
   }
 
   toString() {
