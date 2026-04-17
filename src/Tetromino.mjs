671ff445ead9847b1cdeb45307349c52b5ad0537
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-17 14:20:43 +0300

    refactor: Tetromino constructor takes a single string as parameter

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,25 +1,21 @@
 export class Tetromino {
-  static T_SHAPE = new Tetromino([
-    '.T.',
-    'TTT',
-    '...',
-  ]);
+  static T_SHAPE = new Tetromino(".T.\nTTT\n...");
 
   characters;
 
   constructor (initCharacters) {
-    this.characters = [...initCharacters];
+    this.characters = initCharacters;
   }
 
   toString() {
-    return this.characters.join("\n") + "\n";
+    return this.characters + "\n";
   }
 
   rotateRight() {
-    return new Tetromino(['.T.', '.TT', '.T.'])
+    return new Tetromino(".T.\n.TT\n.T.")
   }
 
   rotateLeft() {
-    return new Tetromino(['.T.', 'TT.', '.T.'])
+    return new Tetromino(".T.\nTT.\n.T.")
   }
 }
