44857a0fc8702b0ade461c845e80e9f87c43b397
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-22 11:15:02 +0300

    refactor: create transpose() function

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,3 +1,7 @@
 export function stringFrom2DArray(arr) {
   return arr.map(row => row.join("")).join("\n") + "\n";
 }
+
+export function transpose(arr) {
+  return arr[0].map((_, i) => arr.map(row => row[i]));
+}
