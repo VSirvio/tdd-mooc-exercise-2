beadb65ee3f4d83835e91ca9b3fc4bdf27717b7b
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-22 11:04:52 +0300

    refactor: modify stringFrom2DArray() to use map() and join()

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,10 +1,3 @@
 export function stringFrom2DArray(arr) {
-  let result = '';
-  for (let y = 0; y < arr.length; ++y) {
-    for (let x = 0; x < arr[0].length; ++x) {
-      result += arr[y][x];
-    }
-    result += "\n";
-  }
-  return result;
+  return arr.map(row => row.join("")).join("\n") + "\n";
 }
