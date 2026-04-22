115a2ae619726f3c4327d382e9eb49c05dd31254
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-22 12:01:02 +0300

    refactor: create utils.composeOver() function

--- src/utils.mjs
+++ src/utils.mjs
@@ -5,3 +5,9 @@ export function stringFrom2DArray(arr) {
 export function transpose(arr) {
   return arr[0].map((_, i) => arr.map(row => row[i]));
 }
+
+export function composeOver(overlay, background, overlayLocation) {
+  const result = background.map(row => [...row]);
+
+  return result;
+}
