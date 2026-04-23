f0d2be245c7c107b322d59ab127165cb7fc86636
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 17:42:31 +0300

    refactor: modify Board.#collides() to Board.#overlaps() (#21)

--- src/Board.mjs
+++ src/Board.mjs
@@ -100,7 +100,7 @@ export class Board {
 
   moveLeft() {
     const newLocation = this.#currentBlockLocation.movedBy(-1, 0);
-    if (!this.#overlaps(this.#currentBlock, this.#currentBlockLocation.movedBy(-1, 0))) {
+    if (!this.#overlaps(this.#currentBlock, newLocation)) {
       this.#currentBlockLocation = this.#currentBlockLocation.movedBy(-1, 0);
     }
   }
