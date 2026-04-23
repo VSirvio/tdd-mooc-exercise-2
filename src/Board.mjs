2430545cde5cbb1d7dcb095d96a34de73b5c42f4
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 17:20:34 +0300

    refactor: create Coord2D class (#26)

--- src/Board.mjs
+++ src/Board.mjs
@@ -73,6 +73,7 @@ export class Board {
           this.#currentBlock.to2DArray(),
           this.#gameArea,
           this.#currentBlockLocation,
+          this.#currentBlockLocation2,
         );
 
         this.#currentBlockLocation = undefined;
