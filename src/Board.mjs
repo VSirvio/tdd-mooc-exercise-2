915c79b9210a69252868957cb4473aa4d57ca4be
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 12:59:25 +0300

    refactor: make member variables private wherever appropriate (#44)

--- src/Board.mjs
+++ src/Board.mjs
@@ -51,7 +51,6 @@ export class Board {
 
     const blockWidth = blockObject.toString().indexOf("\n");
 
-    this.currentBlock = blockObject;
     this.#currentBlock = blockObject;
     this.currentBlockLocation = [Math.floor((this.getWidth() - blockWidth) / 2), 0];
   }
