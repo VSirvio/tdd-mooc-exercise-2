df49f001e7f8b8313d8204824ac92170784f23de
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 11:31:49 +0300

    Create new commit message based on previous in tcr

--- tcr.mjs
+++ tcr.mjs
@@ -15,7 +15,15 @@ function test() {
 
 function commit() {
   console.log("Tests passed -> Commit changes");
-  execSync(logCommand('git commit --all --message="tcr: tests pass"'), {
+  const lastCommitMessage = execSync("git log -1 --pretty=%B", { encoding: "utf8" });
+  const match = lastCommitMessage.match(/\(#(\d+)\)$/);
+  if (!match) {
+    console.log("TCR cancelled. Last commit message has invalid format.");
+    process.exit(1);
+  }
+  const lastNumber = parseInt(match[1]);
+  const newCommitMessage = lastCommitMessage.replace(/\(#\d+\)$/, `(#${lastNumber + 1})`);
+  execSync(logCommand(`git commit --all --message="${newCommitMessage}"`), {
     stdio: "inherit",
   });
 }
