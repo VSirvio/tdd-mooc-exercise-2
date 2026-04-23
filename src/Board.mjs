8e7c15073be8222e102ebaee4c7eb8a9adf6f65e
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 13:59:15 +0300

    refactor: modify Board.#collidesDownward() to Board.#collides() (#2)

--- src/Board.mjs
+++ src/Board.mjs
@@ -64,7 +64,7 @@ export class Board {
 
       let blockBottomMargin = bottomMargin(currentBlock);
 
-      if (this.#collidesDownward()) {
+      if (this.#collidesDownward(0, 1)) {
         this.#gameArea = composeOver(
           currentBlock,
           this.#gameArea,
