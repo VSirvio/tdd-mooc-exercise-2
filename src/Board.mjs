c37d533411fb505d7cf6c2a35654e53d5ac60135
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-23 17:31:50 +0300

    refactor: create Coord2D class (#74)

--- src/Board.mjs
+++ src/Board.mjs
@@ -112,7 +112,6 @@ export class Board {
   moveRight() {
     if (!this.#collides(1, 0)) {
       this.#currentBlockLocation = this.#currentBlockLocation.movedBy(1, 0);
-      this.#currentBlockLocation2 = this.#currentBlockLocation2.movedBy(1, 0);
     }
   }
 
