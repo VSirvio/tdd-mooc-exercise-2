13242ae450ba48dd15b12096a8bbbfd81a22f351
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-24 12:33:18 +0300

    refactor: hard-code orientations in Tetromino class (#24)

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -35,6 +35,7 @@ export class Tetromino {
 
   constructor(rotatingShapes) {
     this.#rotatingShapes = [...rotatingShapes];
+    this.#characterMatrices = [...rotatingShapes];
   }
 
   toString() {
