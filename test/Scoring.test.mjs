cfdae5e687c6dace2ab30ff91646334f92a26a87
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-27 16:09:08 +0300

    feat: the scoring system starts with score 0 (#2)

--- test/Scoring.test.mjs
+++ test/Scoring.test.mjs
@@ -1,1 +1,8 @@
+import { describe, test } from "vitest";
 import { ScoringSystem } from "../src/ScoringSystem.mjs";
+
+describe("The scoring system", () => {
+  test("starts with score 0", () => {
+    const scoring = new ScoringSystem();
+  });
+});
