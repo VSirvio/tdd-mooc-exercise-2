d37dd03a88a2a62158879117c7aa3fef9a5cb449
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-27 10:00:47 +0300

    refactor: create test doubles for tetrominos (#11)

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -46,7 +46,7 @@ describe("Falling tetrominoes", () => {
   test("stop when they land on another block", () => {
     board.drop(TestingTetromino.T_SHAPE);
     fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
+    board.drop(TestingTetromino.T_SHAPE);
     fallToBottom(board);
 
     expect(board.toString()).to.equalShape(
