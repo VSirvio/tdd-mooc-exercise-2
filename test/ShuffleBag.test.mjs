9f28201213b81f613bafb9b028be5017bfe87676
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-28 10:28:47 +0300

    feat: shuffle bag pulls out the one value when the bag has only one value (#2)

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -1,1 +1,8 @@
+import { describe, test } from "vitest";
 import { ShuffleBag } from "../src/ShuffleBag.mjs"
+
+describe("Shuffle bag", () => {
+  test("pulls out the one value when the bag has only one value", () => {
+    const shuffleBag = new ShuffleBag();
+  });
+});
