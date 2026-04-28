81271e1ac39611f5557830111dc4bc060c03d235
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-28 10:48:24 +0300

    feat: shuffle bag pulls out the values that it has been initialized with

--- src/ShuffleBag.mjs
+++ src/ShuffleBag.mjs
@@ -1,5 +1,12 @@
 export class ShuffleBag {
+  values;
+  currentIndex = 0;
+
+  constructor(values) {
+    this.values = values;
+  }
+
   next() {
-    return 473;
+    return this.values[this.currentIndex++];
   }
 }
--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -7,4 +7,14 @@ describe("Shuffle bag", () => {
     const shuffleBag = new ShuffleBag([473]);
     expect(shuffleBag.next()).to.equal(473);
   });
+
+  test("pulls out the values that it has been initialized with", () => {
+    const values = [15, 993, 6];
+    const shuffleBag = new ShuffleBag(values);
+    const pulledOut = [];
+    for (let i = 0; i < values.length; ++i) {
+      pulledOut.push(shuffleBag.next());
+    }
+    expect(pulledOut).to.have.members(values);
+  });
 });
